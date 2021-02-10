4aabb866abe1917c57a7807ac0b27860cd0b89d5
mleclair14 <68674918+mleclair14@users.noreply.github.com>
2021-02-09 19:50:36 -0800

    Update BC_REC_BOT_3.0.py
    trying to bypass heroku timeout

--- BC_REC_BOT_3.0.py
+++ BC_REC_BOT_3.0.py
@@ -26,86 +26,80 @@ PASS_KEY = environ ['PASS_KEY']
 
 def book ():
      #Open login dialog 
-    gChromeOptions = webdriver.ChromeOptions()
-    gChromeOptions.add_argument("window-size=1920x1480")
-    gChromeOptions.add_argument("disable-dev-shm-usage")
-    gDriver = webdriver.Chrome(
-        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-    )
-    gDriver.get("https://recconnect.bc.edu")
-    time.sleep(1)
-    gDriver.execute_script('document.querySelector("#loginLink-mobile").click()')
+    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser.get("https://recconnect.bc.edu")
+    browser.execute_script('document.querySelector("#loginLink-mobile").click()')
     time.sleep(1)
     
     #Login to reconnect.bc.edu
-    username_box = gDriver.find_element_by_css_selector ('#txtUsername')
+    username_box = browser.find_element_by_css_selector ('#txtUsername')
     username_box.send_keys(USERNAME_KEY)
-    password_box = gDriver.find_element_by_css_selector ('#txtPassword')
+    password_box = browser.find_element_by_css_selector ('#txtPassword')
     password_box.send_keys(PASS_KEY)
-    gDriver.find_element_by_css_selector ('#btnLogin').click()
+    browser.find_element_by_css_selector ('#btnLogin').click()
     
 
-    gDriver.execute_script("window.open('');")
-    gDriver.switch_to.window(gDriver.window_handles[1])
-    gDriver.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
-    element = gDriver.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
-    actions = ActionChains(gDriver)
+    browser.execute_script("window.open('');")
+    browser.switch_to.window(gDriver.window_handles[1])
+    browser.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
+    element = browser.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
+    actions = ActionChains(browser)
     actions.move_to_element(element).perform()
     element.click()
-    day = gDriver.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]').get_attribute("data-day")
+    day = browser.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]').get_attribute("data-day")
     initial_day = int(day)
-    gDriver.close()
-    gDriver.switch_to.window(gDriver.window_handles[0])
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])
     
     def check_date ():
-        gDriver.execute_script("window.open('');")
-        gDriver.switch_to.window(gDriver.window_handles[1])
-        gDriver.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
-        element = gDriver.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
-        actions = ActionChains(gDriver)
+        browser.execute_script("window.open('');")
+        browser.switch_to.window(browser.window_handles[1])
+        browser.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
+        element = browser.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
+        actions = ActionChains(browser)
         actions.move_to_element(element).perform()
         element.click()
-        day = gDriver.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]').get_attribute("data-day")
+        day = browser.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]').get_attribute("data-day")
         day = int(day)
         time.sleep(1)
-        gDriver.close()
-        gDriver.switch_to.window(gDriver.window_handles[0])
+        browser.close()
+        browser.switch_to.window(gDriver.window_handles[0])
         return(day)
     
     day = check_date()
     
-    if initial_day == day:
+    while initial_day == day:
         try:
             print(datetime.now())
             print('New gym appointments not yet available.')
-            print('Rechecking in 10 seconds...')
-            time.sleep(10)
+            print('Rechecking...')
+            time.sleep(1)
+            day = check_date()
 
 
         except:
-            print('Rechecking in 10 seconds...')
-            time.sleep(10)
+            print('Rechecking...')
+            time.sleep(1)
             try:
                 day = check_date()
             except:
                 print('ERROR, please restart!')
                 
     #Book Appointment
-    if initial_day == day:
-        try:
-            if (datetime.today().strftime('%A') == 'Monday') or (datetime.today().strftime('%A') == 'Sunday') or (datetime.today().strftime('%A') == 'Saturday') or (datetime.today().strftime('%A') == 'Friday'):
-                gDriver.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
-                element = gDriver.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
-                actions = ActionChains(gDriver)
-                actions.move_to_element(element).perform()
-                element.click()
+    if (datetime.today().strftime('%A') == 'Monday') or (datetime.today().strftime('%A') == 'Sunday') or (datetime.today().strftime('%A') == 'Saturday') or (datetime.today().strftime('%A') == 'Friday'):
+        browser.get('https://recconnect.bc.edu/booking/93a7d382-156f-472c-bf90-006fb897d1c4')
+        element = browser.find_element_by_xpath ('//*[@id="divBookingDateSelector"]/div[2]/div[2]/button[4]')
+        actions = ActionChains(browser)
+        actions.move_to_element(element).perform()
+        element.click()
 
-                last_book = gDriver.find_element_by_css_selector ('#divBookingSlots > div > div:nth-child(9) > div > button')
-                actions = ActionChains(gDriver)
-                actions.move_to_element(last_book).perform()
-                gDriver.find_element_by_css_selector  ('#divBookingSlots > div > div:nth-child(1) > div > button')
-        except:
-            pass
+        last_book = browser.find_element_by_css_selector ('#divBookingSlots > div > div:nth-child(9) > div > button')
+        actions = ActionChains(browser)
+        actions.move_to_element(last_book).perform()
+        browser.find_element_by_css_selector  ('#divBookingSlots > div > div:nth-child(1) > div > button')
+    
+        
+
             
         
         
